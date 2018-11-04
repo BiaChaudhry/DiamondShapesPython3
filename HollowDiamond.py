ee459ca99d29dbe8484c69094c5e128cba36503d
Bia Chaudhry <biach1312@gmail.com>
2018-11-04 15:53:54 +0500

    create HollowDiamond.py
    http://seblog1312.blogspot.com/2018/08/hollow-diamond.html

---
+++ HollowDiamond.py
@@ -0,0 +1,4 @@
+# The respective code can be viewed in my blog post:
+# at http://seblog1312.blogspot.com/2018/08/hollow-diamond.html
+# I have provided the images for the code and the output...
+# Happy Coding!
